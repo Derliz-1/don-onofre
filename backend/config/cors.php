030bd9afd5f276153d7bd2e705c7e00edd677189
Derliz dominguez <Derlizdominguez@gmail.com>
2025-04-29 20:57:18 -0300

    Mejoras en el backend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,21 +2,21 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],  
+'allowed_origins' => ['https://don-onofre-zeta.vercel.app'],
 
-    'allowed_origins_patterns' => [],
+'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+'exposed_headers' => [],
 
-    'max_age' => 0,
+'max_age' => 0,
 
-    'supports_credentials' => false,
+'supports_credentials' => true,
 ];
 
 
